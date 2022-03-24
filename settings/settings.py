635f5ec95734594caa8c2135bbc8b1d7f51eb26f
tomdom80 <tomdom80@gmail.com>
2022-03-24 10:09:35 +0100

    static

--- settings/settings.py
+++ settings/settings.py
@@ -139,6 +139,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    # '/var/www/static/',
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
