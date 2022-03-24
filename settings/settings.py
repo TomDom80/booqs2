a74e6b9711280bd210f799e402623138f865081a
tomdom80 <tomdom80@gmail.com>
2022-03-24 10:29:09 +0100

    static

--- settings/settings.py
+++ settings/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     # '/var/www/static/',
