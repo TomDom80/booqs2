9478ce0af63e2e35817df3c837a4669b6595ef5e
tomdom80 <tomdom80@gmail.com>
2022-03-30 16:27:42 +0200

    origin

--- books/services/import_service.py
+++ books/services/import_service.py
@@ -297,7 +297,7 @@ def save_google_book_obj(book_obj):
             name = ' '.join(sep_auth[0:-1])
             surname = sep_auth[-1]
         elif len(sep_auth) >= 1:
-            surname = sep_auth[0]
+            name = sep_auth[0]
 
         if Author.objects.filter(name=name, surname=surname).exists():
             try:
